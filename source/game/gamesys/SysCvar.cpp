69cf791e672117f78a5132bbb705b053d3ca6fd0
Azuvector <azuvector@gmail.com>
2009-07-04 05:44:08 +0000

    g_useNuclearHammer cvar, enables or disables Hammer/SSM EMP behaviour. Maybe more in future.
    git-svn-id: https://localhost/svn/qwta@293 6eeef719-9b92-4d6a-8419-93dac47c4fdd

--- source/game/gamesys/SysCvar.cpp
+++ source/game/gamesys/SysCvar.cpp
@@ -596,6 +596,7 @@ idCVar	g_useQuake4DarkMatter(		"g_useQuake4DarkMatter",		"1",		CVAR_GAME | CVAR_
 idCVar	g_artilleryWarning(			"g_artilleryWarning",			"0",		CVAR_GAME | CVAR_BOOL | CVAR_ARCHIVE,						"Notify players of the launch of Hammer or DMC artillery." );
 idCVar	g_useDeathFading(			"g_useDeathFading",				"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Use screen fading effects when dead or unconscious?" );
 idCVar	g_useReverseAirstrikes(		"g_useReverseAirstrikes",		"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Use reversed airstrikes on the altfire of the Vampire and Violator?" );
+idCVar	g_useNuclearHammer(			"g_useNuclearHammer",			"1",		CVAR_GAME | CVAR_BOOL | CVAR_ARCHIVE,						"Make the Hammer and SSM missiles behave more like tactical nukes?" );
 
 idCVar	g_useClassLimits(			"g_useClassLimits",				"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Enables or disables class number limitations." );
 
