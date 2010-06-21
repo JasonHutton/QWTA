afe2c69c61b2d27591c5cbc59eee95e04bf4923f
Azuvector <azuvector@gmail.com>
2010-06-21 08:34:15 +0000

    g_useVehicleDecoyAmmo cvar added. QWTA menu. Changelog.
    git-svn-id: https://localhost/svn/qwta@713 6eeef719-9b92-4d6a-8419-93dac47c4fdd

--- source/game/gamesys/SysCvar.cpp
+++ source/game/gamesys/SysCvar.cpp
@@ -629,6 +629,7 @@ idCVar	g_useBaseETQW12SniperTrail( "g_useBaseETQW12SniperTrail",	"1",		CVAR_GAME
 //idCVar	g_hideHud(					"g_hideHud",					"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Disables most aspects of the HUD, revealing them only when they're accessed in some way." );
 idCVar	g_useRealisticWeapons(		"g_useRealisticWeapons",		"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Should weapons behave in a realistic manner, or more game-oriented?" );
 idCVar	g_useVehicleAmmo(			"g_useVehicleAmmo",				"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Should vehicle weapons consume ammunition or not?" );
+idCVar	g_useVehicleDecoyAmmo(		"g_useVehicleDecoyAmmo",		"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Should vehicle decoys consume ammunition or not?" );
 idCVar	g_allowEMPFriendlyFire(		"g_allowEMPFriendlyFire",		"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Allow or disallow EMP effects from affecting friendly units." );
 idCVar	g_allowRadFriendlyFire(		"g_allowRadFriendlyFire",		"1",		CVAR_GAME | CVAR_BOOL | CVAR_NETWORKSYNC,					"Allow or disallow radiation effects from affecting friendly units." );
 
